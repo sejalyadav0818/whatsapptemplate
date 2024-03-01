510f378e7ad0193b508688315d671f46cdffb108
SEJAL YADAV <130818890+sejalyadav0818@users.noreply.github.com>
2024-03-01 18:20:49 +0530

    Update conn.js

--- database/conn.js
+++ database/conn.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
 // require('dotenv').config();
 
-mongoose.connect('mongodb+srv://RahulWaghela:testuser123@cluster0.wcnvqgq.mongodb.net/?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
+.then(() => {
     console.log("Connected successfully.");
 }).catch((err) => {
     console.error("Error connecting to MongoDB:", err);
