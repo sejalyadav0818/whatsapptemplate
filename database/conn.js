a9fc9f63c2f18aa58ae207ba87e56dd4a98af2c6
sejal yadav <sejalyadav0818@gmail.com>
2024-03-02 18:51:19 +0530

    added Report section

--- database/conn.js
+++ database/conn.js
@@ -1,11 +1,23 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
+
+// mongoose.connect('mongodb+srv://RahulWaghela:testuser123@cluster0.wcnvqgq.mongodb.net/?retryWrites=true&w=majority', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// }).then(() => {
+//     console.log("Connected successfully.");
+// }).catch((err) => {
+//     console.error("Error connecting to MongoDB:", err);
+// });
 // require('dotenv').config();
 
-mongoose.connect('mongodb+srv://RahulWaghela:testuser123@cluster0.wcnvqgq.mongodb.net/?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+mongoose.connect('mongodb+srv://sejal:PJLYvnOSauidRbgg@cluster0.cyncaox.mongodb.net/?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 }).then(() => {
-    console.log("Connected successfully.");
+  console.log("Connected successfully.");
 }).catch((err) => {
-    console.error("Error connecting to MongoDB:", err);
+  console.error("Error connecting to MongoDB:", err);
 });
+
+
