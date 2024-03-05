70d042e030e1892aa2580ec3444f20e3e82c48e5
sejal yadav <sejalyadav0818@gmail.com>
2024-03-05 14:47:57 +0530

    added param template

--- index.js
+++ index.js
@@ -23,6 +23,14 @@ app.get("/", (req, res) => {
   }
 });
 
+app.get("/second", (req, res) => {
+  try {
+    res.render("sedemotemplate");
+  } catch (error) {
+    res.status(500).send("internal server Error");
+  }
+});
+
 app.post('/sendtemplateMessage', async (req, res) => {
   try {
     const { recipients, selectTemp } = req.body;
@@ -58,6 +66,177 @@ app.post('/sendtemplateMessage', async (req, res) => {
   } catch (error) {
     console.error('Error sending messages:', error);
     res.status(500).send('Error sending messages');
+  }
+});
+
+app.post('/secondsendtemplateMessage', async (req, res) => {
+  try {
+    const { recipients, selectTemp } = req.body;
+    const phoneNumberArray = recipients.split(',').map(phone => phone.trim());
+    console.log(req.body);
+    console.log(phoneNumberArray, "phoneNumberArray");
+    for (const recipient of phoneNumberArray) {
+      console.log("rec", recipient);
+      const response = await axios.post(
+        'https://graph.facebook.com/v18.0/265933736594328/messages',
+        {
+          messaging_product: "whatsapp",
+          to: recipient,
+          type: "template",
+          template: {
+            name: selectTemp,
+            language: {
+              code: "en"
+            }
+          }
+        },
+        {
+          headers: {
+            Authorization: 'Bearer EAAUZAW89HZBioBOZCFVMcyi55ZBIkZBaQEe8SWFXLHFnUkPnOqVkjE0UdZAQmRNNxrf54Orn31uAXb3O6i9tnqea7BoiyVVBLDGayVZAfXbSBNlEs920fojjvgY0rsatJZAesQI6HXRiXCw2rltS6ycS3akHcvB4qGX6ZCBjPfZBVvE9LjzdZApe94BrsZAZBSwDm6gYg',
+            "Content-Type": "application/json"
+          },
+        }
+      );
+      console.log(`Message sent to ${recipient}`);
+    }
+
+    res.status(200).send('Messages sent successfully');
+  } catch (error) {
+    console.error('Error sending messages:', error);
+    res.status(500).send('Error sending messages');
+  }
+});
+
+
+
+//demo
+// app.post('/secondsendtemplateMessagetry', async (req, res) => {
+//   try {
+//     const { recipients, selectTemp } = req.body;
+//     const phoneNumberArray = recipients.split(',').map(phone => phone.trim());
+//     console.log(req.body, phoneNumberArray);
+//     for (const recipient of phoneNumberArray) {
+//       const response = await axios.post(
+//         'https://graph.facebook.com/v18.0/265933736594328/messages',
+//         {
+//           messaging_product: "whatsapp",
+//           to: recipient,
+//           type: "template",
+//           template: {
+//             name: selectTemp,
+//             language: {
+//               code: "en"
+//             }
+//           }
+//         },
+//         {
+//           headers: {
+//             Authorization: 'Bearer EAAUZAW89HZBioBOZCFVMcyi55ZBIkZBaQEe8SWFXLHFnUkPnOqVkjE0UdZAQmRNNxrf54Orn31uAXb3O6i9tnqea7BoiyVVBLDGayVZAfXbSBNlEs920fojjvgY0rsatJZAesQI6HXRiXCw2rltS6ycS3akHcvB4qGX6ZCBjPfZBVvE9LjzdZApe94BrsZAZBSwDm6gYg', // Replace with your actual access token
+//             "Content-Type": "application/json"
+//           },
+//         }
+//       );
+
+//       console.log(`Message sent to ${recipient}`);
+//     }
+
+//     res.status(200).send('Messages sent successfully');
+//   } catch (error) {
+//     console.error('Error sending messages:', error);
+//     res.status(500).send('Error sending messages');
+//   }
+// });
+// app.post('/secondsendtemplateMessagetry', async (req, res) => {
+//   try {
+//     const { recipients, selectTemp, otpCode } = req.body; // Assuming you're also receiving the OTP code in the request body
+//     console.log(req.body);
+//     const phoneNumberArray = recipients.split(',').map(phone => phone.trim());
+
+//     for (const recipient of phoneNumberArray) {
+//       const response = await axios.post(
+//         'https://graph.facebook.com/v18.0/265933736594328/messages',
+//         {
+//           messaging_product: "whatsapp",
+//           to: recipient,
+//           type: "template",
+//           template: {
+//             name: selectTemp,
+//             language: {
+//               "code": "en"
+//             },
+//             components: [
+//               {
+//                 type: "body",
+//                 "parameters": [
+//                   {
+//                     "type": "text",
+//                      "text": otpCode,
+//                   }
+//                 ]
+//               }
+//             ],
+//           }
+//         },
+//         {
+//           headers: {
+//             Authorization: 'Bearer EAAUZAW89HZBioBOZCFVMcyi55ZBIkZBaQEe8SWFXLHFnUkPnOqVkjE0UdZAQmRNNxrf54Orn31uAXb3O6i9tnqea7BoiyVVBLDGayVZAfXbSBNlEs920fojjvgY0rsatJZAesQI6HXRiXCw2rltS6ycS3akHcvB4qGX6ZCBjPfZBVvE9LjzdZApe94BrsZAZBSwDm6gYg', // Replace with your actual access token
+//             "Content-Type": "application/json"
+//           },
+//         }
+//       );
+//       console.log("gggggggggggggggggggggggg", response);
+//       console.log(`Message sent to ${recipient}`);
+//     }
+
+//     res.status(200).send('Messages sent successfully');
+//   } catch (error) {
+//     console.error('Error sending messages:', error);
+//     res.status(500).send('Error sending messages');
+//   }
+// });
+
+
+app.post('/sendtemplatemessagess/:phoneNumber/:selectTemp/:otpCode', async (req, res) => {
+  try {
+    const { phoneNumber, selectTemp, otpCode } = req.params;
+
+    const response = await axios.post(
+      `https://graph.facebook.com/v18.0/265933736594328/messages?phone=${phoneNumber}`,
+      {
+        messaging_product: "whatsapp",
+        to: phoneNumber,
+        type: "template",
+        template: {
+          name: selectTemp,
+          language: {
+            "code": "en"
+          },
+          components: [
+            {
+              type: "body",
+              "parameters": [
+                {
+                  "type": "text",
+                  "text": otpCode,
+                }
+              ]
+            }
+          ],
+        }
+      },
+      {
+        headers: {
+          Authorization: 'Bearer EAAUZAW89HZBioBOZCFVMcyi55ZBIkZBaQEe8SWFXLHFnUkPnOqVkjE0UdZAQmRNNxrf54Orn31uAXb3O6i9tnqea7BoiyVVBLDGayVZAfXbSBNlEs920fojjvgY0rsatJZAesQI6HXRiXCw2rltS6ycS3akHcvB4qGX6ZCBjPfZBVvE9LjzdZApe94BrsZAZBSwDm6gYg', // Replace with your actual access token
+          "Content-Type": "application/json"
+        },
+      }
+    );
+
+    console.log("Response:", response.data);
+    res.status(200).send('Message sent successfully');
+  } catch (error) {
+    console.error('Error sending message:', error.response.data);
+    res.status(500).send('Error sending message');
   }
 });
 
